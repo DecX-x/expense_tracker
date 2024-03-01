692b7c35a2ff3f88efe0375fbddbdf8b3f10a615
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2024-03-01 08:38:12 +0700

    first commit

---
+++ lib/database/expense-database.dart
@@ -0,0 +1,17 @@
+import 'package:expense_tracker/Models/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ExpenseDatabase extends ChangeNotifier{
+  static late Isar isar;
+
+  List<Expense> _allExpenses = [];
+
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open([ExpenseSchema], directory: dir.path);
+  }
+
+  List<Expense> get allExpense => _allExpenses;
+}
